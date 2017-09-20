ddb8156af9e3357f7f7206d99ea5a045b4e48e1f
Mansour Kheffache <mansourkheffache@gmail.com>
2017-09-20 23:03:20 +0200

    make script read from file

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[29]:
 
 import numpy as np
 
 
-# In[82]:
+# In[30]:
 
-################ 
+################
 ###  CONFIG  ###
 
-# number of states
-n_states = 9
+# read input file
+f = open('input.txt', 'r')
 
-# ok states
-ok_states = [0, 1, 2, 3, 5]
+# first line: number of states
+n_states = int(f.readline())
 
-################    
+# second line: ok states
+ok_states = list(map(int, f.readline().split()))
+
+################
 
 
-# In[ ]:
+# In[31]:
 
 # magic?
 
 # square transition matrix
 t_matrix = np.zeros((n_states, n_states))
+
 
 # transition definition function
 def add_transition(si, sn, p):
@@ -36,21 +40,21 @@ def add_stall():
     s = np.sum(t_matrix, axis=1)
     for i in range(n_states):
         t_matrix[i][i] = 1 - s[i]
-        
+
 # compute probabilities
 def get_probs():
     # 1. trasnpose
     # 2. subtract Pi
     # 3. add a row of P's
     matrix = np.append((t_matrix.T - np.identity(n_states))[:-1], [np.ones(n_states)], axis=0)
-    
+
     # targets
     t = np.zeros(n_states)
     t[-1] = 1
-    
+
     # solve
     probs = np.linalg.solve(matrix, t)
-    
+
     return probs
 
 # compute network overall reliability
@@ -58,51 +62,25 @@ def get_status(probs):
     print(probs)
     prob_good = np.sum(probs[ok_states])
     prob_bad = 1 - prob_good
-    
+
     print("\nOK:" , prob_good)
 
 
-# In[83]:
+# In[32]:
 
 # START Transitions
 
-add_transition(0, 1, 0.4)
-add_transition(0, 2, 0.4)
-
-add_transition(1, 0, 0.3)
-add_transition(1, 3, 0.2)
-add_transition(1, 4, 0.4)
-
-add_transition(2, 0, 0.3)
-add_transition(2, 4, 0.4)
-add_transition(2, 5, 0.2)
-
-add_transition(3, 1, 0.6)
-add_transition(3, 6, 0.4)
-
-add_transition(4, 1, 0.3)
-add_transition(4, 2, 0.3)
-add_transition(4, 6, 0.2)
-add_transition(4, 7, 0.2)
-
-add_transition(5, 2, 0.6)
-add_transition(5, 7, 0.4)
-
-add_transition(6, 3, 0.3)
-add_transition(6, 4, 0.6)
-add_transition(6, 8, 0.2)
-
-add_transition(7, 5, 0.3)
-add_transition(7, 4, 0.6)
-add_transition(7, 8, 0.2)
-
-add_transition(8, 6, 0.6)
-add_transition(8, 7, 0.6)
+# read line by line
+for line in f:
+    if line != '\n':
+        t_info = line.split()
+        # parse transition info
+        add_transition(int(t_info[0]), int(t_info[1]), float(t_info[2]))
 
 # END Transitions
 
 
-# In[ ]:
+# In[33]:
 
 # compute and get overall result
 
@@ -111,4 +89,3 @@ add_stall()
 probs = get_probs()
 
 get_status(probs)
-
